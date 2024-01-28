8fb0e4f1046a634bec65b55b5d89b3935c6009ef
Bluepelota <80494809+Bluepelota@users.noreply.github.com>
2024-01-28 18:53:14 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -3,25 +3,18 @@ import socket
 class Assignment2:
     def __init__(self, year):
         self.year = year
-        
+
     def tellAge(self, currentYear):
         birth_year = currentYear - self.year
         print(f"Your age is {birth_year}")
-        
+
     def listAnniversaries(self):
-        today = 2022
+        today = 2022  
         anniversaries = [i for i in range(10, today - self.year + 1, 10)]
         return anniversaries
-    
+
     def modifyYear(self, n):
         year_str = str(self.year)
-        modified_str = year_str[:2] * n + year_str[2::2][:n]
-        return modified_str
-    
-    @staticmethod
-  def modifyYear(self, n):
-        year_str = str(self.year)
-
         if len(year_str) < 2:
             return "Invalid input"
 
@@ -43,12 +36,20 @@ class Assignment2:
         result = first_two_digits + odd_positioned_chars * n
         return result
 
- 
-        
+    @staticmethod
+    def checkGoodString(string):
+        try:
+            if len(string) >= 9 and string[0].islower() and string.count('0') == 1:
+                return True
+            else:
+                return False
+        except IndexError:
+            return False
+
     @staticmethod
     def connectTcp(host, port):
         try:
-            with socket.create_connection((host, port), timeout = 10) as s:
+            with socket.create_connection((host, port), timeout=5) as s:
                 return True
         except (socket.timeout, socket.error):
             return False
